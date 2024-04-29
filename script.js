4ebb3a0b5d321d0f7b665f0419a6c03f8e13b70d
Abdullateef2006 <119931646+Abdullateef2006@users.noreply.github.com>
2024-04-29 22:44:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// alert("Click on the whatsApp to contact me")
+let toggleBtn = document.getElementById("")
+
 $(document).ready(function () {
 
     $('#menu').click(function () {
@@ -18,21 +21,78 @@ $(document).ready(function () {
 
     });
 
-    // smooth scrolling 
-
-    // $('a[href*="#"]').on('click', function (e) {
-
-    //     e.preventDefault();
-
-    //     $('html, body').animate({
-
-    //         scrollTop: $($(this).attr('href')).offset().top,
-
-    //     },
-    //         500,
-    //         'linear'
-    //     );
-
-    // });
 
 });
+
+function Tap() {
+    document.body.classList.toggle('darkIcon')    
+}
+
+
+
+
+// script.js
+const countElement = document.getElementById("count");
+const targetNumber = 300; // Change this to your desired target number
+const duration = 2000; // Change this to the duration of the counting animation in milliseconds
+const interval = 10; // Interval between each count update
+
+const increment = Math.ceil(targetNumber / (duration / interval));
+let currentCount = 0;
+
+function updateCount() {
+    if (currentCount < targetNumber) {
+        currentCount += increment;
+        if (currentCount > targetNumber) {
+            currentCount = targetNumber;
+        }
+        countElement.innerText = currentCount;
+        setTimeout(updateCount, interval);
+    }
+}
+
+updateCount();
+
+
+
+const words = ["Software developer", "Fullstack developer", "UI/UX designer",  ];
+const wordElement = document.getElementById("word");
+const cursorElement = document.getElementById("cursor");
+let wordIndex = 0;
+let charIndex = 0;
+let typingForward = true;
+
+function typeWord() {
+  if (typingForward) {
+    if (charIndex <= words[wordIndex].length) {
+      wordElement.textContent = words[wordIndex].substring(0, charIndex);
+      charIndex++;
+    } else {
+      typingForward = false;
+      setTimeout(eraseWord, 700);
+      return;
+    }
+  } else {
+    if (charIndex >= 0) {
+      wordElement.textContent = words[wordIndex].substring(0, charIndex);
+      charIndex--;
+    } else {
+      typingForward = true;
+      wordIndex = (wordIndex + 1) % words.length;
+    }
+  }
+  setTimeout(typeWord, 100);
+}
+
+function eraseWord() {
+  if (charIndex >= 0) {
+    wordElement.textContent = words[wordIndex].substring(0, charIndex);
+    charIndex--;
+    setTimeout(eraseWord, 100);
+  } else {
+    wordIndex = (wordIndex + 1) % words.length;
+    setTimeout(typeWord, 700);
+  }
+}
+
+typeWord();
